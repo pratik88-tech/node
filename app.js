2c13a9c0ed9833e9ac29c8bad30caacfecad49eb
pratik88-tech <51715153+pratik88-tech@users.noreply.github.com>
2023-08-15 13:22:01 +0530

    cors added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express=require("express");
 const mongoose = require("mongoose");
 const app =express()
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 const noteRouter = require("./routes/noteRoutes")
 app.get("/",function(req,res){
