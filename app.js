a205ac9b737ea97688e0e07c68cebd3ddae344c6
pratik88-tech <51715153+pratik88-tech@users.noreply.github.com>
2023-08-15 13:17:41 +0530

    app listen updated

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ app.get("/new",function(req,res){
 app.use("/notes", noteRouter)
 mongoose.connect("mongodb+srv://admin:admin@cluster0.m7qftvs.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
-    app.listen(process.env.PORT || 5000,()=>{
-        console.log("server started at port no :5000 ")
-    })
+ 
 })
 .catch((error)=>{
     console.log(error)
 })
-
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("server started at port no :5000 ")
+})
 module.exports=app
