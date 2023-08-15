967bf81473be61916c8cf964d667646886f67eda
pratik88-tech <51715153+pratik88-tech@users.noreply.github.com>
2023-08-15 12:14:33 +0530

    new route added

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const app =express()
 app.get("/",function(req,res){
     res.send("It Worked");
 })
+app.get("/new",function(req,res){
+    res.send("new also Worked");
+})
 app.listen(process.env.PORT || 5000)
 module.exports=app
